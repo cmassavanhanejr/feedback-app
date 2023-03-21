ab9af62f20cbf469093519d26176591547da95ee
Carlos A. Massavanhane Jr <cmassavanhanejr@gmail.com>
2023-03-21 13:33:09 +0200

    finish the functional elements

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
 
-function FeedbackItem(){
-    const [rating, setRating] = useState(7)
+function FeedbackItem({item}){
+    /*const [rating, setRating] = useState(7)
     const [text, setText] = useState('This is an example of feedback item')
 
     //just use to understand the prev attribute from state;
-    /*const handleClick = () => {
+    const handleClick = () => {
         setRating ((prev) => {
             console.log (prev);
             return prev + 0.5
@@ -14,9 +13,9 @@ function FeedbackItem(){
 
     return(
         <div className="card">
-            <div className="num-display">{rating}</div>
-            <div className="text-display">{text}</div>
-            <button onClick={handleClick}>Click</button>
+            <div className="num-display">{item.rating}</div>
+            <div className="text-display">{item.text}</div>
+            {/*<button onClick={handleClick}>Click</button>*/ }
         </div>
     )
 }
